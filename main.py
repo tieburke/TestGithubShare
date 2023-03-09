87f063e8e39bb2b64793db4a6fce4de03401d8c4
OGBK2 <tiernanlburke@gmail.com>
2023-03-08 19:56:45 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("Input Name:")
+
+if name == "Tiernan" or name == "Connor":
+    print("straight")
+
+if name == "Thomas":
+    print("gay")
+
+if name != "Thomas" and name != "Tiernan" and name != "Connor":
+    print("you spelled your name wrong or shouldn't be here")
